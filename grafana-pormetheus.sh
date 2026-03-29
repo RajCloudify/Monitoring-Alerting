5ecdc1e56459c0bc467469eb599f373f4d609df0
Raj Attingal <rajattingal1@gmail.com>
2026-03-29 21:43:50 +0400

    Update grafana-pormetheus.sh

--- grafana-pormetheus.sh
+++ grafana-pormetheus.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-echo "=============================="
+echo "==============================="
 echo " Installing Promethous-grafana-Alertmanager--Node Exporter"
 echo "=============================="
 
